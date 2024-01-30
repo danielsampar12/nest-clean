3d8db07793ab4856b97b2d7ea088f5e787e99c3d
Daniel Sampar <daniel_sampar@hotmail.com>
2024-01-30 18:50:29 -0300

    chore: http module

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -2,13 +2,8 @@ import { Module } from '@nestjs/common'
 
 import { ConfigModule } from '@nestjs/config'
 
-import { PrismaService } from './prisma/prisma.service'
-import { CreateAccountController } from './http/controllers/create-account.controller'
 import { envSchema } from './env'
 import { AuthModule } from './auth/auth.module'
-import { AuthenticateController } from './http/controllers/authenticate.controller'
-import { CreateQuestionController } from './http/controllers/create-question.controller'
-import { FetchRecentQuestionsController } from './http/controllers/fetch-recent-questions.controller'
 
 @Module({
   imports: [
@@ -18,12 +13,5 @@ import { FetchRecentQuestionsController } from './http/controllers/fetch-recent-
     }),
     AuthModule,
   ],
-  controllers: [
-    CreateAccountController,
-    AuthenticateController,
-    CreateQuestionController,
-    FetchRecentQuestionsController,
-  ],
-  providers: [PrismaService],
 })
 export class AppModule {}
